776851e9efd10c6234e8b65de4897d353b520fa3
René Gastmeier <garz@Renes-MBP.fritz.box>
2015-07-10 04:45:49 +0200

    implemented create a mongo db collection async and if it is not already there

--- src/main/java/justweb/services/MongoService.java
+++ src/main/java/justweb/services/MongoService.java
@@ -6,7 +6,11 @@ import com.mongodb.async.SingleResultCallback;
 import com.mongodb.async.client.FindIterable;
 import com.mongodb.async.client.MongoCollection;
 import com.mongodb.async.client.MongoDatabase;
+import com.mongodb.async.client.MongoIterable;
+import justweb.util.Holder;
 import org.bson.Document;
+
+import java.util.function.Consumer;
 
 public class MongoService {
 
@@ -17,15 +21,39 @@ public class MongoService {
     }
 
     @Suspendable
+    public void createCollection(String name) {
+        Holder<Boolean> contained = new Holder<>(false);
+
+        Void v = async(callback -> {
+            MongoIterable<Document> it = db.listCollections();
+            it.forEach(document -> {
+                if (document.get("name").equals(name))
+                    contained.set(true);
+            }, callback);
+        });
+
+        if (! contained.get())
+            asyncVoid(callback -> db.createCollection(name, callback));
+    }
+
+    @Suspendable
     public Document findOne(String collectionName) {
-        Document document = null;
+        return async(callback -> {
+            MongoCollection collection = db.getCollection(collectionName);
+            FindIterable finder = collection.find();
+            finder.first(callback);
+        });
+    }
+
+    @Suspendable
+    private <TResult> TResult async(Consumer<MongoAsync<TResult>> requestAsync) {
+        TResult result = null;
+
         try {
-            document = new MongoAsync<Document>() {
+            result = new MongoAsync<TResult>() {
                 @Override
                 protected void requestAsync() {
-                    MongoCollection collection = db.getCollection(collectionName);
-                    FindIterable finder = collection.find();
-                    finder.first(this);
+                    requestAsync.accept(this);
                 }
             }.run();
         } catch (InterruptedException e) {
@@ -33,13 +61,27 @@ public class MongoService {
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
-        return document;
+
+        return result;
     }
 
-    private abstract class MongoAsync<TResult> extends FiberAsync<TResult, Throwable> implements SingleResultCallback<Document> {
+    @Suspendable
+    private void asyncVoid(Consumer<MongoAsync<Void>> requestAsync) {
+        async(requestAsync);
+    }
+
+    private abstract class MongoAsync<TResult> extends FiberAsync<TResult, Throwable> implements SingleResultCallback<TResult> {
         @Override
-        public void onResult(Document result, Throwable t) {
-            System.err.println(result.toString());
+        public void onResult(TResult result, Throwable t) {
+            if (result != null && t == null) {
+                asyncCompleted(result);
+            }
+            else if (t != null) {
+                asyncFailed(t);
+            }
+            else {
+                asyncCompleted(null);
+            }
         }
     }
 
--- src/main/java/justweb/util/Holder.java
+++ src/main/java/justweb/util/Holder.java
@@ -1,8 +1,15 @@
-package justweb;
+package justweb.util;
 
 public class Holder<T> {
 
     private T held;
+
+    public Holder() {
+    }
+
+    public Holder(T held) {
+        this.held = held;
+    }
 
     public T get() { return held; }
     public void set(T held) { this.held = held; }
